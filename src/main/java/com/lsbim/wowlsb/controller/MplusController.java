2a78a9a111fcd606bbf5f0dc63c4406fcd4a314d
lsbim <rnqjatnqja@gmail.com>
2025-01-26 08:20:31 +0900

    추가: Bucket4j 처리율 제한, 컨트롤러 일부 예외처리, 컨트롤러 요청 로그출력

--- src/main/java/com/lsbim/wowlsb/controller/MplusController.java
+++ src/main/java/com/lsbim/wowlsb/controller/MplusController.java
@@ -3,11 +3,13 @@ package com.lsbim.wowlsb.controller;
 import com.lsbim.wowlsb.dto.ApiResponseDTO;
 import com.lsbim.wowlsb.service.ProcessingService;
 import com.lsbim.wowlsb.service.repository.MplusTimelineDataService;
+import io.github.bucket4j.Bucket;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @CrossOrigin
@@ -17,21 +19,27 @@ import org.springframework.web.bind.annotation.*;
 public class MplusController {
 
     private final MplusTimelineDataService mplusTimelineDataService;
+    private final Bucket bucket;
 
     @GetMapping("timeline")
     public ResponseEntity<?> get(@RequestParam String className,
                                  @RequestParam String specName,
                                  @RequestParam int dungeonId) {
         try {
-            ApiResponseDTO data = mplusTimelineDataService.getTimelineData(className, specName, dungeonId);
+            if (bucket.tryConsume(1)) {
 
-            log.info("get timeline... class: {}, spec: {}, dungeonId: {}", className, specName, dungeonId);
+                ApiResponseDTO data = mplusTimelineDataService.getTimelineData(className, specName, dungeonId);
 
-            return ResponseEntity.ok(data);
+                log.info("get timeline... class: {}, spec: {}, dungeonId: {}", className, specName, dungeonId);
+
+                return ResponseEntity.ok(data);
+            } else {
+                throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS);
+            }
         } catch (NullPointerException e) {
             log.warn("timeline data is null");
             return ResponseEntity.status(HttpStatus.OK)
-                    .body("timeline data is null");
+                    .body("Timeline data is null");
         } catch (Exception e) {
             log.warn("get timeline data error: {}", e);
             return ResponseEntity.status(HttpStatus.OK)
--- src/main/java/com/lsbim/wowlsb/controller/advice/CustomControllerAdvice.java
+++ src/main/java/com/lsbim/wowlsb/controller/advice/CustomControllerAdvice.java
@@ -5,20 +5,37 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class CustomControllerAdvice {
 
-    @Autowired
-    private ObjectMapper om;
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ObjectNode> handleGeneralException(Exception e) {
-        ObjectNode res = om.createObjectNode();
-        res.put("message", "SERVER_ERROR");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+    public ResponseEntity<String> handleGeneralException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Server Error");
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException e) {
+        HttpStatusCode code = e.getStatusCode();
+
+//        Bucket 토큰 과소비하면 투 매니 리퀘스트 status: 429
+        if (code == HttpStatusCode.valueOf(429)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Too Many Request");
+        }
+
+        // 다른 400대 상태 코드에 대한 처리
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getReason());
+    }
+
+    @ExceptionHandler(HttpServerErrorException.ServiceUnavailable.class) // status: 503
+    public ResponseEntity<String> handleUnavailable(HttpServerErrorException.ServiceUnavailable e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Public API Server Error");
     }
 }
