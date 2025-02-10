305a313e468e0b4003ebf5d67b2822974e066320
lsbim <rnqjatnqja@gmail.com>
2025-02-10 18:49:30 +0900

    추가: 처리율 제한 추가
    분당 1000개의 IP를 수용, IP당 초당 5개의 토큰 발급

--- src/main/java/com/lsbim/wowlsb/controller/MplusController.java
+++ src/main/java/com/lsbim/wowlsb/controller/MplusController.java
@@ -1,16 +1,21 @@
 package com.lsbim.wowlsb.controller;
 
+import com.lsbim.wowlsb.controller.advice.RateLimit;
 import com.lsbim.wowlsb.dto.ApiResponseDTO;
 import com.lsbim.wowlsb.service.ProcessingService;
 import com.lsbim.wowlsb.service.repository.MplusTimelineDataService;
 import com.lsbim.wowlsb.service.validation.MplusValidationService;
 import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @CrossOrigin
@@ -21,20 +26,28 @@ public class MplusController {
 
     private final MplusTimelineDataService mplusTimelineDataService;
     private final MplusValidationService mplusValidationService;
-    private final Bucket bucket;
+    private final RateLimit rateLimit;
 
     @GetMapping("timeline")
-    public ResponseEntity<?> get(@RequestParam String className,
+    public ResponseEntity<?> get(HttpServletRequest request,
+                                 @RequestParam String className,
                                  @RequestParam String specName,
                                  @RequestParam int dungeonId) {
 
         if (!mplusValidationService.validateTimelineParams(className, specName, dungeonId)) {
-            return ResponseEntity.status(HttpStatus.OK)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("Invalid params");
         }
 
+        String ip = getClientIp(request);
+
+        if (ip == null || ip.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Ip not found");
+        }
+
         try {
-            if (bucket.tryConsume(1)) {
+            if (rateLimit.tryConsume(ip)) {
                 ApiResponseDTO data = mplusTimelineDataService.getTimelineData(className, specName, dungeonId);
 
                 return ResponseEntity.ok(data);
@@ -44,15 +57,61 @@ public class MplusController {
                 return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
                         .body("Too many request");
             }
+
         } catch (NullPointerException e) {
-            log.warn("timeline data is null", e);
-            return ResponseEntity.status(HttpStatus.OK)
+            log.warn("timeline data is null: class={}, spec={}, dungeon={}",
+                    className, specName, dungeonId, e);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("Timeline data is null");
+
         } catch (Exception e) {
-            log.warn("get timeline data error: {}", e);
-            return ResponseEntity.status(HttpStatus.OK)
+            log.warn("get timeline data error: class={}, spec={}, dungeon={}",
+                    className, specName, dungeonId, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Failed to fetch timeline data");
         }
     }
 
+
+    private String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-RealIP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("REMOTE_ADDR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        if (ip.equals("0:0:0:0:0:0:0:1") || ip.equals("127.0.0.1")) {
+            InetAddress address = null;
+            try {
+                address = InetAddress.getLocalHost();
+            } catch (UnknownHostException e) {
+                log.warn("Unknown Host?: {}", e);
+                throw new RuntimeException(e);
+            }
+            ip = address.getHostName() + "/" + address.getHostAddress();
+        }
+
+        return ip;
+    }
 }
---
+++ src/main/java/com/lsbim/wowlsb/controller/advice/RateLimit.java
@@ -0,0 +1,70 @@
+package com.lsbim.wowlsb.controller.advice;
+
+import io.github.bucket4j.Bucket;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+@Log4j2
+public class RateLimit {
+
+    //    전역 이용자 카운터
+    private final AtomicInteger userIpCounter = new AtomicInteger(0);
+
+    //    IP 안에 버킷 보관
+    private final ConcurrentHashMap<String, Bucket> ipBucket = new ConcurrentHashMap<>();
+
+    private final int TOKEN_PER_SEC = 5;
+    private final int MAX_IP_COUNTER = 1000;
+
+    private Bucket createBucket() {
+
+        return Bucket.builder()
+                .addLimit(limit -> limit
+                        .capacity(TOKEN_PER_SEC) // 버킷의 토큰 최대 용량
+                        .refillGreedy(TOKEN_PER_SEC, Duration.ofSeconds(1)) // 1초에 n개의 토큰 리필
+                        .initialTokens(TOKEN_PER_SEC) // 초기 토큰 n개 발급
+                )
+                .build();
+    }
+
+    //    토큰 소비
+    public boolean tryConsume(String ip) {
+
+        Bucket bucket = ipBucket.computeIfAbsent(ip, k -> {
+//            새 IP 등록 시 카운트 증가
+            if (userIpCounter.incrementAndGet() > MAX_IP_COUNTER) {
+//                제한 초과 시 버킷 생성 거부
+                log.warn("New IP limit exceeded: {}", ip);
+                userIpCounter.decrementAndGet();
+                return null;
+            }
+
+            log.info("New bucket create IP: {}", ip);
+            return createBucket();
+        });
+
+//        버킷이 null이 아니고 토큰 소비가 가능할 경우 true 반환
+        boolean consume = bucket != null && bucket.tryConsume(1);
+
+        if(!consume){
+            log.info("Rate Limit exceeded IP: {}", ip);
+        }
+
+        return consume;
+    }
+
+//    1분마다 맵과 유저카운터 초기화
+    @Scheduled(fixedRate = 60000)
+    public void ipBucketCleanup(){
+        int size = ipBucket.size();
+        ipBucket.clear();
+        userIpCounter.set(0);
+        log.info("Clean up {} IP bucket", size);
+    }
+}
